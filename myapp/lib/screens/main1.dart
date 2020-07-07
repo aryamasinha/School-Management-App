196a5e2b91410e9ead48de54635c32abdda38438
divyamraj18 <divyamraj18@gmail.com>
2020-07-07 09:29:10 +0530

    added native splash screen

--- myapp/lib/screens/main1.dart
+++ myapp/lib/screens/main1.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import './screens/login_screen.dart';
 import 'package:splashscreen/splashscreen.dart';
+
+import 'login_screen.dart';
 
 void main() {
   runApp(new MaterialApp(
